cbdf843d017435455a68bc561f7e3b7423d4ae2e
deniam <deniam24@deniam.com>
2025-03-04 15:04:36 +0000

    fixed search button glitch

--- src/app.js
+++ src/app.js
@@ -1166,7 +1166,10 @@ import {
   
     if (searchButton.textContent === "Stop Search") {
       searchCancelled = true;
-      searchButton.innerHTML = `<svg ...></svg> Search Flights`;
+      searchButton.innerHTML = 
+      `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg> Search Flights`;
       return;
     }
   
@@ -1190,7 +1193,9 @@ import {
   
     if (!originInputRaw) {
       alert("Please enter at least one departure airport.");
-      searchButton.innerHTML = `<svg ...></svg> Search Flights`;
+      searchButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg> Search Flights`;
       return;
     }
   
@@ -1382,7 +1387,9 @@ import {
         document.querySelector(".route-list").innerHTML = "<p>There are no available flights on this route.</p>";
       }
       hideProgress();
-      searchButton.innerHTML = `<svg ...></svg> Search Flights`;
+      searchButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg> Search Flights`;
     }
   }
   
